0d37f7f8d0f0a1c82ec37111145e261bd6f6be54
Maykon Meneghel <maykonmeneghel@icloud.com>
2023-06-01 23:35:38 -0300

    feat: added timer and send message to open the door

--- PocBLE/ContentView.swift
+++ PocBLE/ContentView.swift
@@ -2,30 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: Properties
+    
     let bluetoothService = BluetoothService()
-    
     @State private var message: String = ""
-    
     @State var isConnected: Bool = false
+    @State var isOpened: Bool = false
+    @State private var timer: Timer?
+    @State private var timeRemaining: Int = 30
+    
+    // MARK: Body
     
     var body: some View {
-        VStack(spacing: 32) {
-            Image(systemName: "circle.fill")
-                .resizable()
-                .frame(width: 20, height: 20)
-                .foregroundColor(isConnected ? .green : .red)
-            
-            Button {
-                isConnected.toggle()
-            } label: {
-                Text(isConnected ? "Disconnect" : "Connect")
-                    .font(.body)
-                    .foregroundColor(.primary)
+        VStack() {
+            Spacer()
+            if isConnected { openDoorView }
+            Spacer()
+            HStack(spacing: 16) {
+                indicatorView
+                connectionButtonView
             }
-            .frame(width: 120, height: 40)
-            .background(.secondary)
-            .cornerRadius(20)
-            
         }
         .onChange(of: isConnected, perform: { newValue in
             if newValue {
@@ -34,28 +30,64 @@ struct ContentView: View {
                 bluetoothService.disconnect()
             }
         })
-//
-//            TextField("Digite uma mensagem", text: $message)
-//                .font(.body)
-//                .padding()
-//
-//            Button(action: {
-//                bluetoothService.sendMessage(message)
-//            }) {
-//                Text("Enviar")
-//                    .font(.body)
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .background(Color.blue)
-//                    .cornerRadius(10)
-//            }
-//        }
-//        .onAppear {
-//            bluetoothService.startScanningDevices()
-//        }
-//        .onDisappear {
-//            bluetoothService.stopScanningDevices()
-//        }
-//        .padding()
+    }
+    
+    // MARK: Views
+    
+    private var indicatorView: some View {
+        Image(systemName: "circle.fill")
+            .resizable()
+            .frame(width: 12, height: 12)
+            .foregroundColor(isConnected ? .green : .red)
+    }
+    
+    private var connectionButtonView: some View {
+        Button {
+            resetTimer()
+            isConnected.toggle()
+        } label: {
+            Text(isConnected ? "Disconnect" : "Connect")
+                .font(.body)
+                .foregroundColor(.primary)
+        }
+        .frame(width: 120, height: 40)
+        .background(.secondary)
+        .cornerRadius(20)
+    }
+    
+    private var openDoorView: some View {
+        VStack {
+            Button {
+                bluetoothService.sendMessage("")
+                isOpened = true
+                startTimer()
+            } label: {
+                Image(systemName: isOpened ? "lock.open" : "lock")
+                    .resizable()
+                    .frame(width: isOpened ? 50 : 40, height: 55)
+            }
+            .disabled(timeRemaining < 30)
+            
+            if isOpened {
+                Text("\(timeRemaining)")
+                    .font(.body)
+            }
+        }
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                resetTimer()
+            }
+        }
+    }
+    
+    private func resetTimer() {
+        timeRemaining = 30
+        isOpened = false
+        timer?.invalidate()
     }
 }
